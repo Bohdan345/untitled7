13cb740867dce04e984bed1d3a3a428acebbd016
Bogdan Tyshkovets <btishkovets@gmail.com>
2021-09-09 19:31:52 +0300

    asdasd

---
+++ src/test/java/te.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class te {
+
+
+    @Test
+    public void asdas(){
+        open("https://www.google.com/");
+
+    }
+}
